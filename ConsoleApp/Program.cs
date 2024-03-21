94543fe781d55593d178b4e437c394488f6b11d3
fortraie <kowalewskijakub@icloud.com>
2024-03-21 14:27:30 +0100

    Changing the name of a variable (input --> ints)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,9 +5,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(CalculateAverage([1, 4, 6, 8]));
 Console.WriteLine(CalculateMax([1, 4, 6, 8]));
 
-static double CalculateAverage(int[] input)
+static double CalculateAverage(int[] ints)
 {
-    return input.Average();
+    return ints.Average();
 }
 
 static double CalculateMax(int[] input)
