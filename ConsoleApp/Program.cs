f2c7c5a6a0168a9e94eadd39ffbde0e5d949734a
fortraie <kowalewskijakub@icloud.com>
2024-03-21 14:14:35 +0100

    Implementation of requirements and change of project's name

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 3rd change
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(CalculateAverage([1, 4, 6, 8]));
+
+static double CalculateAverage(int[] input)
+{
+    return input.Average();
+}
